56d567ca62f996a29a76ae2e8c165d2edd68a19e
LORDofDOOM <LORDofDOOM@xxx.xxx>
2013-06-24 00:25:00 +0200

    [GTDB] Fix crash

---
+++ GTDB/zzz_custom_fixes.sql
@@ -0,0 +1,2 @@
+-- Fix pinfo crash
+UPDATE trinity_string SET content_loc3=NULL WHERE entry IN (453, 548, 549, 550, 714, 716, 749, 752, 843, 844, 845, 846, 847, 848, 849, 850, 851, 852, 853, 854, 855, 856); 
